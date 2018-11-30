b1fed6853e47ce8de6d21e604c57df8f9261f8dc
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2018-11-30 14:47:28 +0100

    Update provision.sh

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,24 +1,23 @@
 #!/usr/bin/env bash
 
-apt-get update -y
-apt-get install vim -y
-apt-get install curl -y
+# Install tools
+apt-get update
+apt-get install -y vim curl unzip
 
-# Install terraform and docker CE
-
+# Install terraform
 curl -O https://releases.hashicorp.com/terraform/0.11.10/terraform_0.11.10_linux_amd64.zip
 apt-get install unzip
 mkdir /bin/terraform 
 unzip terraform_0.11.10_linux_amd64.zip -d /usr/local/bin/
 
-apt-get update -y
-apt-get install apt-transport-https -y 
-apt-get install ca-certificates -y
-apt-get install software-properties-common -y
+# Insall docker
+apt-get install -y apt-transport-https ca-certificates software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg |   apt-key add -
 add-apt-repository \
 "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) \
 stable"
-apt-get update -y
-apt-get install docker-ce -y
+
+apt-get update
+apt-get install -y docker-ce
+# end
